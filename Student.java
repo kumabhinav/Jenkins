69f5056d1c8b4869ff0433251f3044e523664cbb
Kumar Abhinav <68700523+kumabhinav@users.noreply.github.com>
2023-09-18 15:04:14 +0530

    C4

---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    public Student(){
+        System.out.println("Creating student class");
+    }
+
+    public void sayHello(){
+        System.out.println("Hii, I'm a student");
+    }
+}
