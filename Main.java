f806b4cec5701e282482c330889c5e1937740ee9
Kumar Abhinav <68700523+kumabhinav@users.noreply.github.com>
2023-09-18 14:36:36 +0530

    C3

--- Main.java
+++ Main.java
@@ -5,5 +5,7 @@ public class Main {
         System.out.println("Hello");
         System.out.println(new Date());
         System.out.println("Date");
+        Student s1 = new Student();
+        s1.sayHello();
     }
 }
